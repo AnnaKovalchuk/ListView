22af00f1684f81c44ee772a2528f3ebc8f4baea6
anaykovalchuk00@mail.ru <anaykovalchuk00@mail.ru>
2022-03-16 13:15:56 +0300

    textview

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onCreate(Bundle savedInstanceState)
-    {
+    {//
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String[] colors =getResources().getStringArray(R.array.cat_names);
